d5d6c9aa92c56e69d6d43c9b54fb60d4eadedb03
srikarrohit <srikarrohit@gmail.com>
2015-06-27 13:19:23 +0530

    mailerfunction

--- newuser.php
+++ newuser.php
@@ -9,7 +9,6 @@ $email=$_POST["email"];
 $add=$_POST["address"];  
 $phone=$_POST["phone"];  
 $conn0 = new mysqli($server, $user, $pass, $db);
-// Check connection
 if ($conn0->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
@@ -17,19 +16,10 @@ if ($conn0->connect_error) {
 $sql = "SELECT * FROM newloginform WHERE email='{$email}'";
 $result = $conn0->query($sql);
 if ($result->num_rows == 0) {
-    // output data of each row
 $conn0->close();
 try	{
 	$conn=new PDO("mysql:host=$server;dbname=$db", $user, $pass);
-    // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-//	$stmt0 = $conn->prepare("SELECT email from newloginform WHERE email=:email");
-//	$stmt0->bindParam(':email', $email);
- // $stmt0->execute();
-//  $result = $stmt0->setFetchMode(PDO::FETCH_ASSOC); 	
-//	echo $result;
-//	if ($result == null )
-//	{
   	$stmt = $conn->prepare("INSERT INTO newloginform (name, shop, email,address,phone) 
 		VALUES (:name, :shop, :email,:add,:phone)");
     $stmt->bindParam(':name', $name);
@@ -37,26 +27,47 @@ try	{
     $stmt->bindParam(':email', $email);
 		$stmt->bindParam(':add', $add); 
 		$stmt->bindParam(':phone', $phone); 
-    // insert a row
 		$stmt1 = $conn->prepare("INSERT INTO confirmedform (username,password) 
 		VALUES (:user,:pass)");
     $stmt1->bindParam(':user', $name);
-		$pass=$name . date('mY');
-		$pass=md5($pass);
-    $stmt1->bindParam(':pass', $pass);
-    // insert a row
-    $stmt1->execute();
+		$password=generatePassword();
+    $stmt1->bindParam(':pass', $password);
+     $stmt1->execute();
 		$stmt->execute();
-//		$stmt0->execute();
-//	$emailverify='';
-// 	$stmt0->bind_result($emailverify)	;
-//	$stmt0->fetch();
-//	echo $emailverify;	 
-//		}
-//	else
-//	{
-//		echo "Email has already been taken";
-//	}
+echo (extension_loaded('openssl')?'SSL loaded':'SSL not loaded')."\n";
+$message = "Your username is".$name."/nYour Password is".$password;
+require("PHPMailer-master/PHPMailerAutoload.php");
+
+$mail = new PHPMailer();
+
+$mail->IsSMTP();
+$mail->SMTPDebug = 1;
+$mail->SMTPSecure = 'tls';
+$mail->Host = "smtp.gmail.com"; 
+$mail->Port = 587;
+$mail->SMTPAuth = true;   
+$mail->Username = "";  
+$mail->Password = ""; 
+$mail->setFrom = "";
+$mail->addReplyTo("");
+$mail->AddAddress($email);
+
+$mail->WordWrap = 50;
+$mail->IsHTML(true);
+
+$mail->Subject = "You have received feedback from your website!";
+$mail->Body    = $message;
+$mail->AltBody = $message;
+
+if(!$mail->Send())
+{
+   echo "Message could not be sent. <p>";
+   echo "Mailer Error: " . $mail->ErrorInfo;
+   exit;
+}
+
+echo "Message has been sent";
+
    }
 catch(PDOException $e)
     {
@@ -67,31 +78,16 @@ else
 {
 echo "email taken";
 }
-
-//$sql="SELECT id FROM newloginform WHERE name=$name";
-//$result = $conn->query($sql);
-//$row = $result->fetch_assoc();
-//$id=$row["id"];
 $conn=null;
-/*echo $id;
-try	{
-	$conn=new PDO("mysql:host=$server;dbname=$db", $user, $pass);
-    // set the PDO error mode to exception
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $stmt = $conn->prepare("INSERT INTO confirmedform (user_id,username,password) 
-		VALUES (:id,:user,:pass)");
-		$stmt->bindParam(':id', $id);
-    $stmt->bindParam(':user', $name);
-		$pass=$name . date('mY');
-		$pass=md5($key);
-    $stmt->bindParam(':pass', $key);
-    // insert a row
-    $stmt->execute();
-		echo "New record created successfully";
+	function generatePassword($length = 8) {
+    $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    $count = mb_strlen($chars);
+
+    for ($i = 0, $result = ''; $i < $length; $i++) {
+        $index = rand(0, $count - 1);
+        $result .= mb_substr($chars, $index, 1);
     }
-catch(PDOException $e)
-    {
-    echo "Error: " . $e->getMessage();
-    }
-$conn = null;*/
-?> 
+
+    return $result;
+}
+?>
